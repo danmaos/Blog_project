bfe2fa8d9eb0001d602761807cdf3935e65b08e8
DanPC <daniyar.f96@gmail.com>
2022-12-21 05:15:01 +0600

    added allowed hosts

--- Blog_project/Blog_project/settings.py
+++ Blog_project/Blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sm-74*_55n1y14p!ft+(j+zg&)iiq6qmpdse%4kaa201b=%i94
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
 
 # Application definition
